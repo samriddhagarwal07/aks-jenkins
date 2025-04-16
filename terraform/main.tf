6c1b814a0851a6b65a1b70e8e23f888ac791c313
samriddhagarwal07 <samriddh005@gmail.com>
2025-04-16 15:09:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,15 @@
- provider "azurerm" {
+provider "azurerm" {
   features {}
   subscription_id = "4a6f3be4-14cd-451b-97b0-d315131d91cd"
 }
 
+# Create Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
+# Create Azure Container Registry (ACR)
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg.name
@@ -16,6 +18,7 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
+# Create AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_name
   location            = azurerm_resource_group.rg.location
@@ -37,8 +40,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
+# Assign AcrPull Role to AKS kubelet identity
 resource "azurerm_role_assignment" "acr_pull" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.object_id
   role_definition_name            = "AcrPull"
   scope                           = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
