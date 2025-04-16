ba2a3c6d61b7beb4aa3f2b13a2abb72750871565
samriddhagarwal07 <samriddh005@gmail.com>
2025-04-16 16:20:23 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,49 +1,43 @@
 provider "azurerm" {
   features {}
-  subscription_id = "4a6f3be4-14cd-451b-97b0-d315131d91cd"
+  subscription_id = var.subscription_id
 }
 
-# Create Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+resource "azurerm_resource_group" "rg"{
+    name = var.resource_group_name
+    location = var.location
 }
 
-# Create Azure Container Registry (ACR)
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-  admin_enabled       = true
+  resource_group_name = var.resource_group_name
+  location = var.location
+  name = var.acr_name
+  sku = "Basic"
+  admin_enabled = true
 }
 
-# Create AKS Cluster
+
+//az aks create --resource-group rg-docker --name aks-dotnetwebapi --node-count 2 --node-vm-size Standard_B2ms --generate-ssh-keys --attach-acr acrkeshav070425
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "dns-${var.aks_name}"
+  location = var.location
+  resource_group_name = var.resource_group_name
+  name = var.aks_name
+  dns_prefix = var.dns_prefix
 
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    vm_size = "Standard_B2ms"
+    node_count = 2
+    name = "default"
   }
-
+  
   identity {
     type = "SystemAssigned"
   }
-
-  tags = {
-    environment = "dev"
+  
+  depends_on = [ azurerm_container_registry.acr ]
+  
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
   }
-}
-
-# Assign AcrPull Role to AKS kubelet identity
-resource "azurerm_role_assignment" "acr_pull" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name            = "AcrPull"
-  scope                           = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
 }
