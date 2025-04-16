c583c077952bdbe40117d41ec2616e7975bbf955
samriddhagarwal07 <samriddh005@gmail.com>
2025-04-16 15:14:51 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # Assign AcrPull Role to AKS kubelet identity
 resource "azurerm_role_assignment" "acr_pull" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.object_id
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name            = "AcrPull"
   scope                           = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
